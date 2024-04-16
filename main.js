2c9e460e671bfa9710b1a75aa372bf62a2453835
Sana Tehniat <sanaaziz181995>
2024-04-17 02:41:23 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: (chalk.magentaBright.italic("Enter your sentence to count the word: "))
+    }
+]);
+let words = (answers.sentence.trim().split(" "));
+console.log(words);
+console.log(chalk.blueBright.bold(`your sentence word count is ${words.length}`));
